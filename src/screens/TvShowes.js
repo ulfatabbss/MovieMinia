5cd000921f990a2bec6db7e006061427f064a5b7
awais <awaishanifpk@gmail.com>
2023-05-26 18:49:49 +0500

    Styling Bottom Tab Bar

--- src/screens/TvShowes.js
+++ src/screens/TvShowes.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { black } from '../utillis/colors'
 
 const TvShowes = () => {
   return (
-    <View >
-      <Text>TvShowes</Text>
+    <View style={{flex:1, backgroundColor: black, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize:20, color: '#fff'}}>Tv Showes</Text>
     </View>
   )
 }
---
+++ src/utillis/colors.js
@@ -0,0 +1,4 @@
+const Primary = '#E7442E';
+const white='#fff';
+const black='#000'
+export {Primary, white, black};
